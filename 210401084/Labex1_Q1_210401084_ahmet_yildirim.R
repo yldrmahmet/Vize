0e9a1e58f9910fae0c6710bc8ac6aa6a077a07b1
yldrmahmet <ay689089@gmail.com>
2023-11-16 17:46:41 +0300

    1.1, testleri ile tamamlandı

---
+++ 210401084/Labex1_Q1_210401084_ahmet_yildirim.R
@@ -0,0 +1,4 @@
+# Dosyayı İndirme
+url <- "https://raw.githubusercontent.com/scizmeli/Red/master/MapsThatChangedOurWorld_StoryMap_Data.csv"
+download.file(url, destfile = "MapsThatChangedOurWorld_StoryMap_Data.csv", mode = "wb")
+
---
+++ 210401084/Labex1_Q1_tests_210401084_ahmet_yildirim.R
@@ -0,0 +1,22 @@
+library(testthat)
+
+test_that("Workspace'teki tüm değişkenleri silme", {
+  rm(list = ls())
+  expect_equal(length(ls()), 0)
+})
+
+test_that("Eğer mevcut ise veri setini silme", {
+  if ("MapsThatChangedOurWorld_StoryMap_Data.csv" %in% list.files()) {
+    file.remove("MapsThatChangedOurWorld_StoryMap_Data.csv")
+  }
+  expect_equal("MapsThatChangedOurWorld_StoryMap_Data.csv" %in% list.files(), FALSE)
+})
+
+current_dir <- getwd()
+relative_path <- file.path(current_dir,"Labex1_Q1_210401084_ahmet_yildirim.R")
+source(relative_path)
+
+test_that("Test : MapsThatChangedOurWorld_StoryMap_Data.csv adlı dosya belirtilen dizinde mevcuttur.", {
+  file_path <- file.path(current_dir,"MapsThatChangedOurWorld_StoryMap_Data.csv")
+  expect_true(file.exists(file_path), info = "Dosya mevcut değil.")
+})
