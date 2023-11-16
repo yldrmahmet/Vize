a65be6d1984e6c13feaa4ec48fe7f7a6701c6cba
yldrmahmet <ay689089@gmail.com>
2023-11-16 20:04:13 +0300

    1.7 testleri ile tamamlandi

--- 210401084/Labex1_Q1_210401084_ahmet_yildirim.R
+++ 210401084/Labex1_Q1_210401084_ahmet_yildirim.R
@@ -22,3 +22,7 @@ maps$Longitude <- gsub("[EW]", "", maps$Longitude)
 
 # Year sütunundaki "AD" harflerini silme
 maps$Year <- gsub("AD", "", maps$Year)
+
+#1.7
+maps$Latitude <- as.numeric(gsub("[NW]", "", maps$Latitude))
+maps$Longitude <- as.numeric(gsub("[EW]", "", maps$Longitude))
--- 210401084/Labex1_Q1_tests_210401084_ahmet_yildirim.R
+++ 210401084/Labex1_Q1_tests_210401084_ahmet_yildirim.R
@@ -40,3 +40,7 @@ test_that("Test : maps adlı data.frame'in 5. sütununun adında 'Title' kelimes
 test_that("Test : Latitude adlı sütun numeric değerlerden oluşmalıdır.", {
   expect_is(maps$Latitude, "numeric", info = "Latitude sütunu numeric değerlerden oluşmuyor.")
 })
+
+test_that("Test : Longitude adlı sütun numeric değerlerden oluşmalıdır.", {
+  expect_is(maps$Longitude, "numeric", info = "Longitude sütunu numeric değerlerden oluşmuyor.")
+})
